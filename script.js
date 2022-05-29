c766d1e1f11dff905a5801a82235371d98f7d1cd
Minku Singh <mailminkusingh@gmail.com>
2022-05-29 14:39:23 +0530

    feat: update delete

--- script.js
+++ script.js
@@ -1,76 +1,128 @@
-let modal = document.querySelector(".modal");
-let btnAddBook = document.querySelector("#btn-add-book");
-let btnSubmitBook = document.querySelector("#btn-submit-book");
-let title = document.querySelector("#title");
-let author = document.querySelector("#author");
-let pages = document.querySelector("#pages");
-let read = document.querySelector("#read");
-let main = document.querySelector("main");
-
-
-let flag = false;
-
-btnAddBook.addEventListener("click", () => {
-    flag=!flag;
-
-    if(flag === true){
-        modal.style.display = "block";
-    }else{
-        modal.style.display = "none"
-    }
-});
-
 let library = [];
-
-function Book(title, author, pages, read){
+function Book(title, author, pages, status){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.status = status;
 }
 
-function addBookToLibrary(book){
-    library.push(book);
-    document.querySelector(".book-count").textContent = `${library.length}`;
-    document.querySelector(".read-count").textContent = `${library.length}`;
-    document.querySelector(".not-read-count").textContent = `${library.length}`;
+// selectors ---------------------------------------------------------------
+let addBookBtn = document.querySelector("#add-book");
+let modal = document.querySelector(".modal");
+let form = document.querySelector("#form");
+let valid = document.querySelector(".valid");
+let main = document.querySelector("main");
+let bookCount = document.querySelector(".book-count");
+let readCount = document.querySelector(".read-count");
+let notReadCount = document.querySelector(".not-read-count");
 
-    let newBookTile = document.createElement("section");
-    newBookTile.setAttribute("class", "book");
 
-    newBookTile.innerHTML = `
-        <p>Title: <span class="title">${book.title}</span></p>
-        <p>Author: <span class="author">${book.author}</span></p>
-        <p>Pages: <span class="pages">${book.pages}</span></p>
-        <div class="icons">
-            <i class="fa-solid fa-check-double"></i>
-            <i class="fa-solid fa-trash-can trash"></i>
-        </div>
-    `
 
-    main.appendChild(newBookTile);
-    modal.style.display = "none";
-    flag = false;
-}
+// event listeners ---------------------------------------------------------
+addBookBtn.addEventListener("click", () => toggleModal())
+form.addEventListener("submit", (e) => submitForm(e));
 
-btnSubmitBook.addEventListener("click", (e) => {
-    e.preventDefault();
-    let titleVal = title.value;
-    let authorVal = author.value;
-    let pagesVal = pages.value;
-    let readVal = read.checked;
 
-    if(titleVal === "" || authorVal === "" || pagesVal === "" || readVal === ""){
-        alert("please enter complete details");
-        return;
+// event handlers-----------------------------------------------------------
+let toggleModal = () => {
+    if(modal.style.display === "block"){
+        modal.style.display = "none";
+    }else{
+        modal.style.display = "block";
     }
+}
 
-    let book = new Book(titleVal, authorVal, pagesVal, readVal);
-    
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    read.checked = false;
+let submitForm = (e) => {
+    e.preventDefault();
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let status = document.querySelector("#status").checked;
 
-    addBookToLibrary(book)
-})
+    if(isValid(title, author, pages)){
+        valid.innerHTML = "book added to the library...";
+        setTimeout(() => {
+            valid.innerHTML = "";
+        }, 1500);
+
+        clearInput();
+        toggleModal();
+        addBook(title, author, pages, status);
+    }else{
+        valid.innerHTML = "incomplete details...";
+        setTimeout(() => {
+            valid.innerHTML = "";
+        }, 1500);
+    }
+}
+
+let isValid = (title, author, pages) => {
+    if(title === "" || author === "" || pages === ""){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+let clearInput = () => {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#pages").value = "";
+}
+
+let addBook = (title, author, pages, status) => {
+    let book = new Book(title, author, pages, status);
+    library.push(book);
+
+    addBookTile(book);
+}
+
+let addBookTile = (book) => {
+    main.innerHTML += `
+        <section class="book">
+            <p>Title: <span class="title">${book.title}</span></p>
+            <p>Author: <span class="author">${book.author}</span></p>
+            <p>Pages: <span class="pages">${book.pages}</span></p>
+            <div class="icons">
+                <i onClick = "handleCheckToggle(this, book)" class="check fa-solid fa-check-double"></i>
+                <i onClick = "handleDelete(this)" class="delete fa-solid fa-trash-can trash"></i>
+            </div>
+        </section>
+    `;
+
+    colorCheckBox(book);
+}
+
+let colorCheckBox = (book) => {
+    let checkList = document.querySelectorAll(".check");
+
+    for(let i = 0; i < checkList.length; i++){
+        if(book.status === true){
+            checkList[i].setAttribute("id", "check-green");
+        } 
+    }
+}
+
+let handleDelete = (e) => {
+    // library.remove(book);
+    // console.log(library);
+    // for(let i = 0; i < library.length; i++){
+    //     if(library[i].title === book.title){
+    //         library.splice(i)
+    //     }
+    // }
+    // console.log(library);
+    e.parentElement.parentElement.remove();
+}
+
+let handleCheckToggle = (e) => {
+    if(!(e.getAttribute("id") === "check-green")){
+        e.setAttribute("id", "check-green");
+    }else{
+        e.setAttribute("id", null);
+    }
+}
+
+// let libraryLog = () => {
+
+// }
